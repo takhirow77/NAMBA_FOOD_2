11d26a2442bef55f5a1910fe45f585299f4ced58
takhirow77 <tahirows77@icloud.com>
2024-06-01 10:56:04 +0300

    commit

--- core/settings.py
+++ core/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     #apps
-    'apps.todolist',
+    'apps.about',
 ]
 
 MIDDLEWARE = [
@@ -137,13 +137,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CKEDITOR_UPLOAD_PATH = "uploads/"
-CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'  # URL to jQuery
-CKEDITOR_IMAGE_BACKEND = "pillow"  # Путь к пакету Pillow для обработки изображений
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': 'Custom',  # Вы можете настроить свою собственную панель инструментов CKEditor
-        'height': 300,
-        'width': 800,
-    },
-}
+
--- core/urls.py
+++ core/urls.py
@@ -20,13 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-api_urlpatterns = [
-    path('todolist/', include('apps.todolist.urls')),
-]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(api_urlpatterns)),
+    path('api/v1/about/', include('apps.about.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
